235bed96e55224ad5cb5b0dec171a67568cfd290
DeveshIIITD <devesh14042007@gmail.com>
2026-07-04 20:40:04 +0530

    Implement palindrome check for linked list

---
+++ Palindrome_LL.cpp
@@ -0,0 +1,33 @@
+#include<stack>
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        stack<int> st;
+        bool s = true;
+        ListNode* temp = head;
+        while(temp != NULL){
+            st.push(temp->val);
+            temp = temp->next;
+        }
+        temp = head;
+        while(temp != NULL){
+            if(st.top() != temp->val){
+                s = false;
+                return s;
+            }
+            st.pop();
+            temp = temp->next;
+        }
+        return s;
+    }
+};
