f8a24c761e31232f5afa6398a5b043f770044937
DeveshIIITD <devesh14042007@gmail.com>
2026-07-11 14:27:36 +0530

    Add function to increment value of linked list
    Implemented a function to add one to a singly-linked list, handling carry-over.

--- Add1toLL.cpp
+++ Add1toLL.cpp
@@ -1,2 +1,35 @@
-cout<<"helloo";
-        
+/**
+ * Definition for singly-linked list.   
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {     
+public:
+    int helper(ListNode* temp){
+            if(temp == NULL){  // base case of recursion
+                    return 1;
+            }
+            int carry = helper(temp->next);
+            temp->val = temp->val + carry;
+            if(temp->val < 10){ // no carry
+                    return 0;
+            }
+            temp->val = 0; // there is a carry
+            return 1;
+    }
+    ListNode* add1LL(ListNode* head) {
+            int carry = helper(head);
+            if(carry == 1){ // if there is an extra digit after addition
+                    ListNode* newnode = new ListNode(1);
+                    newnode->next = head;
+                    head = newnode;
+                    return head;
+            }
+            return head;
+        }
+};        
