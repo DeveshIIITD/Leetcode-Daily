47580bc4624de3630a46100f1d9c62a5ef461b4c
DeveshIIITD <devesh14042007@gmail.com>
2026-07-17 13:58:01 +0530

    Handle negative goal case in func method
    Add check for negative goal in func method

--- BinaryArrayswithsum.cpp
+++ BinaryArrayswithsum.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     int func(vector<int>& nums, int goal){
-        
+        if(goal < 0){
+            return 0;
+        }
         int l = 0, r = 0, sum = 0, cnt = 0;
         while(r < nums.size()){
             sum = sum + nums[r];
