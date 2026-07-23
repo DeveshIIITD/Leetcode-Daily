db86dc5cb0ef86c0b48e7e2682940bfc660c54fa
DeveshIIITD <devesh14042007@gmail.com>
2026-07-23 23:10:19 +0530

    Update number-of-substrings-containing-all-three-characters.cpp

--- number-of-substrings-containing-all-three-characters.cpp
+++ number-of-substrings-containing-all-three-characters.cpp
@@ -5,7 +5,7 @@ public:
         int cnt = 0;
         for(int i = 0; i < s.size(); i++){
             lastseen[s[i] - 'a'] = i;
-            if(lastseen[0] != -1 && lastseen[1] != -1 && lastseen[2] != -1){
+            if(lastseen[0] != -1 && lastseen[1] != -1 && lastseen[2] != -1){    
                 int minel = min(lastseen[0],lastseen[1]);
                 cnt = cnt + (1 + min(minel,lastseen[2]));
             }                   
