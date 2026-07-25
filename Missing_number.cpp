c6ab1d18d7df8f1a0d2ea63f8659a4d223990820
DeveshIIITD <devesh14042007@gmail.com>
2026-07-25 23:45:51 +0530

    Update Missing_number.cpp

--- Missing_number.cpp
+++ Missing_number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int res = nums.size();
+        int res = nums.size();    
         
         for (int i = 0; i < nums.size(); i++) {
             res += i - nums[i];
