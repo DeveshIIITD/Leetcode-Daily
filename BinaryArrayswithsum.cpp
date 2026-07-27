ee6b94d77d51be16cb3d8f0a6378078d70badc9f
DeveshIIITD <devesh14042007@gmail.com>
2026-07-27 22:59:03 +0530

    Update BinaryArrayswithsum.cpp

--- BinaryArrayswithsum.cpp
+++ BinaryArrayswithsum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int func(vector<int>& nums, int goal){
         if(goal < 0){
-            return 0;
+            return 0;     
         }        
         int l = 0, r = 0, sum = 0, cnt = 0;     
         while(r < nums.size()){
