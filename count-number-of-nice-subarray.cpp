62c89a866efea4e5b36e6533659282f614a0d1ae
DeveshIIITD <devesh14042007@gmail.com>
2026-07-27 23:04:59 +0530

    Update count-number-of-nice-subarray.cpp

--- count-number-of-nice-subarray.cpp
+++ count-number-of-nice-subarray.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int func(vector<int>& nums, int k){
         if(k < 0){
-            return 0;
+            return 0;     
         }
         int l = 0, r = 0, sum = 0, cnt = 0;
         while(r < nums.size()){
