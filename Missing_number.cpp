5c5c141affc58a618f5fde2fd7d30f4f9b872031
DeveshIIITD <devesh14042007@gmail.com>
2026-08-03 23:04:45 +0530

    Update Missing_number.cpp

--- Missing_number.cpp
+++ Missing_number.cpp
@@ -4,7 +4,7 @@ public:
         int res = nums.size();    
                 
         for (int i = 0; i < nums.size(); i++) {    
-            res += i - nums[i];
+            res += i - nums[i];     
         }
         
         return res;
